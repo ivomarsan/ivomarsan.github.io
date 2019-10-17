47a6f232f26937bc3fb5587b4d707c8df4f46b1b
ivomarsan <ivomarlucio@gmail.com>
2019-10-17 03:34:06 -0300

    Install postcss-custom-media

--- postcss.config.js
+++ postcss.config.js
@@ -6,12 +6,14 @@ module.exports = {
     cssnano: {
       // preset: 'advanced' // npm install cssnano-preset-advanced --save-dev
     },
+    // CSS4+ See more in https://preset-env.cssdb.org/features
     'postcss-preset-env': {
       stage: 2, // See about Stages in https://cssdb.org/#staging-process
       features: {
         'nesting-rules': true
       }
     },
-    'postcss-color-mod-function': {}
+    'postcss-color-mod-function': {},
+    'postcss-custom-media': {}
   }
 }
